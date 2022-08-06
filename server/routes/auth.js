eefe3e8e8cfd51cfcb955f29b36f46de1df6a06a
Bilel Aoun <59199755+Spittzzer@users.noreply.github.com>
2022-08-06 15:26:50 +0200

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -22,22 +22,7 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-// router.post("/signup", async (req, res) => {
-//   try {
-//     const salt = await bcrypt.genSalt(10);
-//     const hashedPass = await bcrypt.hash(req.body.password, salt);
-//     const newUser = new User({
-//       username: req.body.username,
-//       email: req.body.email,
-//       password: hashedPass,
-//     });
 
-//     const user = await newUser.save();
-//     res.status(200).json(user);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 //login
 router.post("/login", async (req, res) => {
